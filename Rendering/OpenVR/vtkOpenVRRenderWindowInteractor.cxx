de3d5104732710a7dd320064d9e06f00c37db32a
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2023-04-11 12:25:18 +0200

    [Backport] OpenVR: Never ignore VREvent_Quit

--- Rendering/OpenVR/vtkOpenVRRenderWindowInteractor.cxx
+++ Rendering/OpenVR/vtkOpenVRRenderWindowInteractor.cxx
@@ -137,6 +137,11 @@ void vtkOpenVRRenderWindowInteractor::DoOneEvent(vtkVRRenderWindow* renWin, vtkR
     // eat up any pending events
     while (pHMD->PollNextEvent(&event, sizeof(vr::VREvent_t)))
     {
+      if (event.eventType == vr::VREvent_Quit)
+      {
+        this->Done = true;
+        return;
+      }
     }
   }
   else
@@ -146,6 +151,11 @@ void vtkOpenVRRenderWindowInteractor::DoOneEvent(vtkVRRenderWindow* renWin, vtkR
     // process all pending events
     while (result)
     {
+      if (event.eventType == vr::VREvent_Quit)
+      {
+        this->Done = true;
+        return;
+      }
       result = pHMD->PollNextEvent(&event, sizeof(vr::VREvent_t));
     }
 
